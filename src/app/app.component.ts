c433ed79ef8c08d139cc303c1724abf997a0a230
b@sh <ded2280@yandex.ru>
2022-06-20 18:42:00 +0300

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div [ngClass]="{verdanaFont:true}" ngClass="{asd:true}">
-    <a routerLink="">Главная </a>
+    <a routerLink="">Главная!! </a>
     <a routerLink="/about">О нас</a>
-  
+
   <router-outlet></router-outlet>
   </div>
-    
+
   `,
   styles: [`
   .verdanaFont { font-weight: normal; font-size:35px; margin:15px; text-align:right; background:whitesmoke;height:auto;}
-  
+
   `]
 })
 export class AppComponent {
